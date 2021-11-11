c4be734db45e42b3684019399f899d2a1ca5d978
Naito Kenta <k-naito@ostechnology.co.jp>
2021-11-11 17:58:51 +0900

    a

--- src/components/Practice/Form.tsx
+++ src/components/Practice/Form.tsx
@@ -4,40 +4,40 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-type Input = {
-  required: string;
-  maxLength: string;
-  maxNumber: number;
-};
+// type Input = {
+//   required: string;
+//   maxLength: string;
+//   maxNumber: number;
+// };
 
-export const Form = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Input>({
-    criteriaMode: "all",
-    shouldFocusError: false,
-  });
-  const onSubmit: SubmitHandler<Input> = (data) => console.log(data);
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* trueの場合必須 */}
-      <br />
-      <input {...register("required", { required: true })} />
-      {errors.required && "文字が入力されていません"}
-      <br />
-      {/* 5文字以上の文字列 */}
-      <br />
-      <input {...register("maxLength", { maxLength: 5 })} />
-      {errors.maxLength && "5文字以上が入力されています"}
-      <br />
-      10以内ならOK
-      <br />
-      <input type="number" {...register("maxNumber", { max: 10 })} />
-      {errors.maxNumber && "10以上の数字が入力されています"}
-      <br />
-      <input type="submit" />
-    </form>
-  );
-};
+// export const Form = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<Input>({
+//     criteriaMode: "all",
+//     shouldFocusError: false,
+//   });
+//   const onSubmit: SubmitHandler<Input> = (data) => console.log(data);
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       {/* trueの場合必須 */}
+//       <br />
+//       <input {...register("required", { required: true })} />
+//       {errors.required && "文字が入力されていません"}
+//       <br />
+//       {/* 5文字以上の文字列 */}
+//       <br />
+//       <input {...register("maxLength", { maxLength: 5 })} />
+//       {errors.maxLength && "5文字以上が入力されています"}
+//       <br />
+//       10以内ならOK
+//       <br />
+//       <input type="number" {...register("maxNumber", { max: 10 })} />
+//       {errors.maxNumber && "10以上の数字が入力されています"}
+//       <br />
+//       <input type="submit" />
+//     </form>
+//   );
+// };
