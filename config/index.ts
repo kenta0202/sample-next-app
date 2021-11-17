21a35f88787b543411eaef55e2eb693f973c5aac
Naito Kenta <k-naito@ostechnology.co.jp>
2021-11-17 17:56:50 +0900

    a

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-// export const server = dev ? "http://localhost:3000" : "yoursite.com";
-export const server = "http://localhost:3000";
+export const server = dev
+  ? "http://localhost:3000"
+  : "sample-next-app-38ame0dhc-kenta0202.vercel.app";
